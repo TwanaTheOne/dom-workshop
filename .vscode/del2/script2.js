08922ce38cf5f439ec346051bb933bcef41f9c2b
karam shaafout <karamshaafout07@gmail.comgit config --global user.email karamshaafout07@gmail.com>
2025-10-16 12:19:15 +0200

    hi

---
+++ .vscode/del2/script2.js
@@ -0,0 +1,15 @@
+// navigator: zaida , twana
+// driver: karam
+
+const rubrik = document.getElementsByName("rubrik");
+const container = document.getElementsByName("control");
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btn1 = document.querySelector("#btn1");
+    btn1.addEventListener("click", () => {
+    body.style.backgroundColor = "#fbbf24";
+    });
+});
